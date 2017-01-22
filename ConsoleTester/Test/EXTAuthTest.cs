c0d39b9355d0751b1dc794514349e3187c5f1e7e
Simon J Butler <SimonButler@users.noreply.github.com>
2017-01-22 22:44:57 +1100

    Revert "Revert "Started adding some basic tests to the project. Will create tests in line with everything else later (once i learn how to)""
    This reverts commit 077bda61a4eaa27a379023e6f78d13fa273d92a7.

---
+++ ConsoleTester/Test/EXTAuthTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class EXTAuthTest
+    {
+        [Fact]
+        public void Test1-1()
+        {
+            Assert.True(true);
+        }
+    }
+}
