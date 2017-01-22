215ef02ff26c66a0d61b0f337edd7f29a50830fa
Simon J Butler <SimonButler@users.noreply.github.com>
2017-01-22 21:38:11 +1100

    Started adding some basic tests to the project. Will create tests in line with everything else later (once i learn how to)

---
+++ ConsoleTester/Test/AuthenticationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class AuthenticationTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
